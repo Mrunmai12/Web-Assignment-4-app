d7bb8945521f08fe3ef69f612012eccac356eab7
Mrunmai12 <67316169+Mrunmai12@users.noreply.github.com>
2020-09-07 00:15:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       }                 
        </style>
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-       <link rel = "stylesheet" href = "css.css">  
+       /*<link rel = "stylesheet" href = "css.css"> */ 
     </head>
     
     <body>
@@ -43,7 +43,7 @@
     </select>
     <br>
   
-    <table id="tabl" class="table thead-dark table-striped table-hover table-borderless">
+    <table id="tabl" class="table thead-light table-dark table-hover table-borderless">
       
       <tr>
         <th>ID</th>
